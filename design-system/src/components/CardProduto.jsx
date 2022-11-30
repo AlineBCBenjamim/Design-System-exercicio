5046304ce15de5812625a737fe62fe191da48a83
Aline Benjamim <alineoak9@gmail.com>
2022-11-30 17:20:19 -0300

    Exercicio Finalizado

---
+++ design-system/src/components/CardProduto.jsx
@@ -0,0 +1,79 @@
+import {
+  Box,
+  Center,
+  useColorModeValue,
+  Heading,
+  Text,
+  Stack,
+  Image,
+} from "@chakra-ui/react";
+
+
+
+export default function ProductSimple({produto}) {
+  const IMAGE = produto.photo
+
+  return (
+    <Center py={12}>
+      <Box
+        role={"group"}
+        p={6}
+        maxW={"400px"}
+        w={"full"}
+        bg={useColorModeValue("pink")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        pos={"relative"}
+        zIndex={1}
+      >
+        <Box
+          rounded={"lg"}
+          mt={-12}
+          pos={"relative"}
+          height={"500px"}
+          _after={{
+            transition: "all .3s ease",
+            content: '""',
+            w: "full",
+            h: "full",
+            pos: "absolute",
+            top: 5,
+            left: 0,
+            backgroundImage: `url(${IMAGE})`,
+            filter: "blur(15px)",
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: "blur(20px)",
+            },
+          }}
+        >
+          <Image
+            rounded={"lg"}
+            height={500}
+            width={382}
+            objectFit={"cover"}
+            src={IMAGE}
+          />
+        </Box>
+        <Stack pt={10} align={"center"}>
+          <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+            {produto.brand}
+          </Text>
+          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+            {produto.name}
+          </Heading>
+          <Stack direction={"row"} align={"center"}>
+            <Text fontWeight={800} fontSize={"xl"}>
+              {`R$ ${produto.priceDiscount}`}
+            </Text>
+            <Text textDecoration={"line-through"} color={"gray.600"}>
+              {`R$ ${produto.priceNormal}`}
+            </Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Center>
+  );
+}
